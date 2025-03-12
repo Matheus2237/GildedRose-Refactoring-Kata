d9995581f0c81a65774efac526083e5331238e30
Matheus2237 <mathpaulinor@gmail.com>
2025-03-12 19:50:37 -0300

    feat: Decréscimo de qualidade em 4 para Conjured Mana Cake

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -45,10 +45,14 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
+                        if (!items[i].name.equals("Conjured Mana Cake")) {
+                            if (items[i].quality > 0) {
+                                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                                    items[i].quality = items[i].quality - 1;
+                                }
                             }
+                        } else {
+                            items[i].quality = items[i].quality - 2;
                         }
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
